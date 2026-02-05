00300769dd7a78f8025cb2a71da74860595629aa
Felix Beer <dev@felix.beer>
2026-02-06 00:14:17 +0100

    feat: add error as default schema component

--- src/common/openapi.rs
+++ src/common/openapi.rs
@@ -1,5 +1,9 @@
+use crate::common::error::ErrorResponse;
 use utoipa::OpenApi;
 
 #[derive(OpenApi)]
-#[openapi(info(title = "TrailSense API", version = "0.1.0"), components())]
+#[openapi(
+    info(title = "TrailSense API", version = "0.1.0"),
+    components(schemas(ErrorResponse))
+)]
 pub struct ApiDoc;

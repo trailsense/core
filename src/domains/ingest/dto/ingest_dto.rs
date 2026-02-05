16ceef7e3421b7b14eae9ee53cc7fa04721aa751
Felix Beer <dev@felix.beer>
2026-02-05 18:25:27 +0100

    feat: add maximum amount in age_in_seconds (1year)

--- src/domains/ingest/dto/ingest_dto.rs
+++ src/domains/ingest/dto/ingest_dto.rs
@@ -15,5 +15,7 @@ pub struct MeasurementDto {
 pub struct IngestDto {
     pub node_id: Uuid,
     pub count: u32,
+    #[schema(maximum = 31_536_000)]
+    #[validate(range(max = 31_536_000))] // 1 year
     pub age_in_seconds: u32,
 }
